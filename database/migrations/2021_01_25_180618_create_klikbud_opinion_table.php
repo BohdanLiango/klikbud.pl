6d440a36e300ccb8a5407b785d5b9ed7f9ce5218
BohdanLiango <bohdan.liango@gmail.com>
2021-01-25 20:58:34 +0100

    nn

--- database/migrations/2021_01_25_180618_create_klikbud_opinion_table.php
+++ database/migrations/2021_01_25_180618_create_klikbud_opinion_table.php
@@ -19,9 +19,12 @@ class CreateKlikbudOpinionTable extends Migration
             $table->integer('service_id')->unsigned()->nullable();
             $table->tinyInteger('stars')->unsigned()->nullable();
             $table->tinyInteger('status_to_main_page_id')->unsigned()->nullable(); // 1 - Active in main page 2 - Hidden
+            $table->tinyInteger('is_new_id')->unsigned()->default(false);
+            $table->integer('portal_opinion_id')->unsigned()->default(NULL);
             $table->integer('user_id')->unsigned()->nullable();
             $table->integer('moderated_id')->nullable();
             $table->mediumText('opinion')->nullable();
+            $table->dateTime('date_add')->nullable();
             $table->softDeletes();
             $table->timestamps();
         });
